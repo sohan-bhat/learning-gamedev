d99f82777a90c99c558c6fc8c666af9a627053f4
sohan-bhat <64918912+sohan-bhat@users.noreply.github.com>
2020-05-17 17:37:16 -0500

    Made the ball have gravity and bounce on hit

--- My Custom Game/ball and paddell gamedev.py
+++ My Custom Game/ball and paddell gamedev.py
@@ -1,5 +1,4 @@
-import pygame
-import random
+import pygame, random, math
 
 pygame.init()
 
@@ -20,6 +19,7 @@ ballImg = pygame.image.load('ball.png')
 ballX = (random.randint(0, 735))
 ballY = (random.randint(50, 150))
 ballImg = pygame.transform.scale(ballImg, (50, 50))
+ballYVelocity = 0
 
 
 # Functions
@@ -29,18 +29,26 @@ def paddle(x, y):
 
 def ball(x, y):
     screen.blit(ballImg, (ballX, ballY))
+
+def isCollision(ballX, ballY, paddleX, paddleY):
+    distance = math.sqrt(math.pow(ballX-paddleX,2))+ (math.pow(ballY-paddleY,2))
+    if distance < 50:
+        return True
+    else: return False
+
 running = True
 while running:
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                paddleX_change = 1
+                paddleX_change = 2
 
             if event.key == pygame.K_LEFT:
-                paddleX_change = -1
+                paddleX_change = -2
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -52,13 +60,17 @@ while running:
     if paddleX <= 0: 
         paddleX = 0
     elif paddleX >= 736:
-        playerX = 736        
+        playerX = 736
+
+    collision = isCollision(ballX, ballY, paddleX, paddleY)
+    if collision:
+        ballYVelocity = -ballYVelocity
+        
 
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
     paddle(paddleX, paddleY)
     ball(ballX, ballY)
+    ballYVelocity += 0.01
+    ballY += ballYVelocity
     pygame.display.update()
-
-    
-    
